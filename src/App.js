aeeb027d5a54f754989b9a4bbf20b5dc569937f3
Samson Ocran <samson.ocran@gmail.com>
2023-06-27 02:34:15 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
+  const course = "Half Stack application development";
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
+
+// header component
+  const Header = (props) => {
+    return (
+      <h1>{props.course}</h1>
+    )
+  }
+
+  // Parts component
+  const Parts = (props) => {
+    return (
+      <p>{props.parts} {props.exercises}</p>
+    )
+  }
+  
+  // contents component
+  const Contents = () => {
+    
+    return (
+      <>
+        <Parts parts={part1} exercises={exercises1} />
+        <Parts parts={part2} exercises={exercises2} />
+        <Parts parts={part3} exercises={exercises3} />
+      </>
+    );
+  };
+
+  // total component
+  const Total = (props) => {
+    return (
+      <p>The Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+    )
+  }
+  
+  // Rendering all componts
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header course={course} />
+      <Contents />
+      <Total
+        exercises1={exercises1}
+        exercises2={exercises2}
+        exercises3={exercises3}
+      />
     </div>
   );
 }
