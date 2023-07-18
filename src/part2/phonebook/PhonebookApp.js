005e8e7eafeba0a5b1ca3ee8affebab12b87288d
Samson Ocran <samson.ocran@gmail.com>
2023-07-18 21:42:54 +0100

    phonebook updated

--- src/part2/phonebook/PhonebookApp.js
+++ src/part2/phonebook/PhonebookApp.js
@@ -27,28 +27,35 @@ const PhonebookApp = () => {
   const [filteredNames, setFilteredNames] = useState([]);
 
   const handleChange = (e) => {
-    const { name, value } = e.target;
-
-    setValues({ ...values, [name]: value });
-
+    const { value } = e.target;
     // Filter the names based on the current input
     const filteredPersons = persons.filter((person) =>
       person.name.toLowerCase().includes(value.toLowerCase())
     );
-
     // Update the filtered names state
     setFilteredNames(filteredPersons);
+  };
+
+  const handleNameAndNum = (e) => {
+    const { name, value } = e.target;
+    setValues({ ...values, [name]: value });
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
 
+    // Validation if name or number already exist
     const warningMsg = persons.some(
-      (person) => person.number === values.number
+      (person) => person.number === values.number || person.name === values.name
     );
 
+    // Validation for empty input fild
+    const emptyInput = values.name === "" || values.number === "";
+
     if (warningMsg) {
-      window.alert("Sorry this number is been registered");
+      window.alert("Sorry this information already exist pls try another");
+    } else if (emptyInput) {
+      window.alert("Input must not be empty");
     } else {
       let personObj = {
         name: values.name,
@@ -64,15 +71,30 @@ const PhonebookApp = () => {
     <div>
       <h2>Phonebook</h2>
       <Filter onChange={handleChange} />
-      <PersonsFrom onSubmit={handleSubmit} textValue={values.name} textOnchange={handleChange} numValue={values.number} numOnchange={handleChange} />
+      <PersonsFrom
+        onSubmit={handleSubmit}
+        textValue={values.name}
+        textOnchange={handleNameAndNum}
+        numValue={values.number}
+        numOnchange={handleNameAndNum}
+      />
       <h2>Numbers</h2>
-        {filteredNames.length > 0 ? (
-          filteredNames.map((person) => (
-            <Persons key={person.id} name={person.name} number={person.number} />
+      
+      {filteredNames.length > 0
+        ? filteredNames.map((person) => (
+            <Persons
+              key={person.id}
+              name={person.name}
+              number={person.number}
+            />
           ))
-        ) : (
-          <p><span>{persons.name}</span> <span>{persons.number}</span></p>
-        )}
+        : persons.map((person) => (
+            <Persons
+              key={person.id}
+              name={person.name}
+              number={person.number}
+            />
+          ))}
     </div>
   );
 };
